2553b546cc950f28dec62d3d05407d10d8b92984
maxhurst <maxhurst@seattleacademy.org>
2024-01-18 09:49:11 -0800

    fix

--- script.js
+++ script.js
@@ -1,28 +1,75 @@
 const numberBox = document.getElementById('guess');
 const feedbackBox = document.getElementById('feedback');
+const closeBox = document.getElementById('closeb');
+const close2Box = document.getElementById('closeb2');
+
 
 const newNumber = Math.ceil(Math.random()*1000);
 
 numberBox.addEventListener('change',checkNumber);
 
 function checkNumber() {
+    const guess = numberBox.value;
+    const closel = newNumber - guess; 
+    const closeh = guess - newNumber;
+    const closen = 20;
+   
+    
+    
 
-const guess = numberBox.value;
 if (guess < newNumber) {
     feedbackBox.innerText= "Too Low!";
 
-}
-else if  ( guess> newNumber ){
+    
+
+     
+        if (closel < closen){
+        closeBox.innerText = "you are pretty close though...";
+    }
+
+    }
+    
+
+else if  ( guess > newNumber ){
 
     feedbackBox.innerText= "Too high!";
+       
+       if (closeh < closen){
+      closeBox.innerText = "you are pretty close though...";
     
-}
+    }
+    }
+    
+
+
 else {
 
     feedbackBox.innerText= "Correct! reload page to start again.";
+    closeBox.innerText = "your as close as you can get";
+   
 
 }
 
 
 }
 
+
+
+
+
+const inputBox = document.getElementById('input');
+const outputBox = document.getElementById('output');
+
+
+inputBox.addEventListener('change',mathfunc);
+
+function mathfunc() {
+
+const input = inputBox.value;
+
+
+outputBox.innerText = input**2;
+
+
+
+}
